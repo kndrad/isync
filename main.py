151371c165841bce0e92280376c51a7e53464d34
kndrad <kon.now@protonmail.com>
2024-09-20 12:36:24 +0200

    IN PROGRESS: Fetching passwords dir.

--- main.py
+++ main.py
@@ -23,17 +23,21 @@ class Config:
             if not os.path.isdir(passwords_dir):
                 print(f"Error: the directory '{passwords_dir}' does not exist.")
 
-            self.passwords_dir = passwords_dir
+            self.local_passwords_dir = passwords_dir
+            self.icloud_passwords_dir = cfg['paths']['icloud_passwords_dir']
 
 def read_config(path:str) -> Config:
     config = Config(path)
     return config
 
+
 if __name__ == '__main__':
-    # Credentials can be read from keyring.
-    # "Authentication will expire after an interval set by Apple,
-    # at which point you will have to re-authenticate.
-    # This interval is currently two months."
+    # Credentials can be read from keyring as well.
+    #
+    # Docs of pyicloud:
+    #   "Authentication will expire after an interval set by Apple,
+    #   at which point you will have to re-authenticate.
+    #   This interval is currently two months."
     config = read_config('./config_dev.yaml')
 
     apple_id = config.apple_id
@@ -49,7 +53,7 @@ if __name__ == '__main__':
     if not api.is_trusted_session:
         api.trust_session()
 
-    if not config.passwords_dir:
+    if not config.local_passwords_dir:
         # Specify the path to the password directory.
         passwords_dir = input("enter path to the passwords directory:")
         passwords_dir = rm_tilde(passwords_dir)
@@ -58,6 +62,11 @@ if __name__ == '__main__':
         if not os.path.isdir(passwords_dir):
             print(f"Error: the directory '{passwords_dir}' does not exist.")
             exit(1)
+
+    icloud_passwords_dir = config.icloud_passwords_dir
+
+    if icloud_passwords_dir in api.drive.dir():
+        print(f"Found icloud passwords dir: '{icloud_passwords_dir}'.")
 
     print("Program completed succesfully.")
 
